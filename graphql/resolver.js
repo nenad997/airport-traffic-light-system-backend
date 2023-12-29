9d10959ec954adb0ed83c27d6d9b417afa4172cb
Nenad <nenad.matijevic97@hotmail.com>
2023-12-29 14:47:36 +0100

    Add sort logic

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -64,7 +64,7 @@ module.exports = {
     };
   },
   getFlights: async () => {
-    const flights = await Flight.find();
+    const flights = await Flight.find().sort({ createdAt: -1 });
 
     if (!flights || flights.length === 0) {
       const error = new Error("Could not fetch flights");
@@ -84,13 +84,15 @@ module.exports = {
   deleteFlight: async ({ flightId }, req) => {
     const foundFlight = await Flight.findOne({ _id: flightId });
 
-    if(!foundFlight) {
-      const error = new Error("You are trying to delete a flight that does not exist");
+    if (!foundFlight) {
+      const error = new Error(
+        "You are trying to delete a flight that does not exist"
+      );
       error.code = 204;
       throw error;
     }
 
     const deletionResult = await Flight.deleteOne({ _id: flightId });
     return deletionResult;
-  }
+  },
 };
